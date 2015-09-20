d2f969a9ac9af1c5d25e85268845ab9770dfdd41
AndersonUFF <anderson_smash@hotmail.com>
2015-09-20 11:26:28 -0300

    Add class Turma.java e AlunoTestDrive modificado

--- EXERCICIOS/Estruturado/Alunos/AlunoTestDrive.java
+++ EXERCICIOS/Estruturado/Alunos/AlunoTestDrive.java
@@ -6,7 +6,8 @@ public class AlunoTestDrive{
 
 		Scanner sc = new Scanner(System.in);
 		Aluno[] novoAluno = new Aluno[MAX];
-		
+		Double totalMedias = 0.0;
+
 		for(byte i = 0; i < MAX; i++){
 			
 			novoAluno[i] = new Aluno();
@@ -14,6 +15,7 @@ public class AlunoTestDrive{
 			System.out.println("Digite o nome do Aluno");
 			novoAluno[i].nome = sc.next();
 
+			/*COLOCAR VIRGULA AO INVÉS DE PONTO EX: 9,5 E NÃO 9.5*/
 			System.out.println("Digite a nota da primeira prova");
 			novoAluno[i].notaP1 = sc.nextFloat();
 			
@@ -21,10 +23,16 @@ public class AlunoTestDrive{
 			novoAluno[i].notaP2 = sc.nextFloat();
 
 			novoAluno[i].mediaAluno = novoAluno[i].calculaMedia();
-
+			totalMedias += novoAluno[i].mediaAluno;
 		}
+
+		Turma mediaTurma = new Turma();
+		mediaTurma.mediaAlunos = totalMedias;
+		mediaTurma.totalAlunos = MAX;
 
 		for(byte i = 0; i < MAX; i++)
 			novoAluno[i].imprimeAluno();
+
+		System.out.println("A Media da Turma foi: " + mediaTurma.calculaMediaTurma());
 	}
 }
---
+++ EXERCICIOS/Estruturado/Alunos/Turma.java
@@ -0,0 +1,8 @@
+public class Turma {
+	Double mediaAlunos;
+	int totalAlunos;
+
+	public Double calculaMediaTurma(){
+		return mediaAlunos / totalAlunos;
+	}
+}
